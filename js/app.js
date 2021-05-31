b6b1a2e1d14e6816efa1a09773afe85c6133c114
rmilosavljevic <71175068+rmilosavljevic@users.noreply.github.com>
2021-05-31 22:00:52 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,41 @@
+
+let lightMode = localStorage.getItem("lightMode");
+const lightModeToggle = document.querySelector("#light-mode-toggle");
+
+
+
+
+//Check if light mode in enabled
+//if it is enabled turn it off
+//if it is disabled turn it on
+
+
+//reusable as many times as we want
+const enableLightMode = () => {
+  //1. add the class darkmode to the body
+  document.body.classList.add('body-light-theme');
+  //2. update lightMode in the Local Storage, so that we know that setting is for next time somebody comes
+  localStorage.setItem('lightMode', 'enabled'); //key-lightMode, value-enabled
+  //3.
+
+}
+
+//opposite of the previous function 
+const disableLightMode = () => {
+  //1. add the class darkmode to the body
+  document.body.classList.remove('body-light-theme');
+  //2. update lightMode in the Local Storage, so that we know that setting is for next time somebody comes
+  localStorage.setItem('lightMode', null); //key-lightMode, value-enabled
+  //3.
+
+}
+
+
+lightModeToggle.addEventListener("click", () => {
+  lightMode = localStorage.getItem('lightMode');
+  if (lightMode !== 'enabled') {
+    enableLightMode();
+  } else {
+    disableLightMode();
+  }
+});
